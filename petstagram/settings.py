cb778e5be17a4b832fa76fe815c3cf8edcc15ea3
unknown <lyudmila_ds@abv.bg>
2021-08-22 23:16:41 +0300

    Added update

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7vn!-(h8-e6)5j-!n(1(g=27-u(gmnz90qm80qbo$t4^f8+wv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
