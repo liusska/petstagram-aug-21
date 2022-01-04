4c09c999fda78eb7b3974c2e43dee50ca393c838
unknown <lyudmila_ds@abv.bg>
2022-01-04 23:04:33 +0200

    Set accounts app, register with email

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from petstagram.accounts.managers import PetstagramUserManager
 
-# Create your models here.
+
+class PetstagramUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = PetstagramUserManager()
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'petstagram.common',
     'petstagram.pets',
+    'petstagram.accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media_files'
 
+AUTH_USER_MODEL = 'accounts.PetstagramUser'
