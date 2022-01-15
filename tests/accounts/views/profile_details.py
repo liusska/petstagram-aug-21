d3d2e8f6e5fe5370f97c352cbcd606b55c7aa468
unknown <lyudmila_ds@abv.bg>
2022-01-16 00:14:17 +0200

    Added test for accounts views

---
+++ tests/accounts/views/profile_details.py
@@ -0,0 +1,77 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from petstagram.pets.models import Pet
+from petstagram.accounts.models import Profile
+
+UserModel = get_user_model()
+
+
+class PetstagramTestCase(TestCase):
+    def assertListEmpty(self, ll):
+        return self.assertListEqual([], ll, 'The list is not empty')
+
+
+class ProfileDetailsTest(PetstagramTestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = UserModel.objects.create_user(email='liusska@mail.bg', password='123456')
+
+    def test_getDetails_when_loggedInUserWithNoPets_shouldGetDetailsWithNoPets(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('profile details'))
+
+        pets = list(response.context['pets'])
+        profile = response.context['profile']
+
+        self.assertListEmpty(pets)
+        self.assertEqual(self.user.id, profile.user_id)
+
+    def test_getDetails_when_loggedInUserWithPets_shouldGetDetailsWithPets(self):
+        pet = Pet.objects.create(
+            name='Test Pet',
+            description='Test pet desc',
+            age='1',
+            image='path/to/image.png',
+            type=Pet.TYPE_CHOICE_DOG,
+            user=self.user,
+        )
+
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse('profile details'))
+
+        self.assertEqual(self.user.id, response.context['profile'].user_id)
+        self.assertListEqual([pet], list(response.context['pets']))
+
+    def test_postDetails_whenUserLoggenInWithoutImage_shouldChangeImage(self):
+        path_to_image = 'path/to/image.png'
+        self.client.force_login(self.user)
+
+        response = self.client.post(reverse('profile details'), data={
+            path_to_image: 'path/to/image.png',
+        })
+
+        self.assertEqual(302, response.status_code)
+
+        profile = Profile.objects.get(pk=self.user.id)
+        # self.assertEqual(path_to_image, profile.profile_image.url)
+
+    def test_postDetails_whenUserLoggenInWithImage_shouldChangeImage(self):
+        path_to_image = 'path/to/image.png'
+        profile = Profile.objects.get(pk=self.user.id)
+        profile.profile_image = path_to_image + 'old'
+        profile.save()
+
+        self.client.force_login(self.user)
+
+        response = self.client.post(reverse('profile details'), data={
+            path_to_image: 'path/to/image.png',
+        })
+
+        self.assertEqual(302, response.status_code)
+
+        profile = Profile.objects.get(pk=self.user.id)
+        # self.assertEqual(path_to_image, profile.profile_image.url)
+
